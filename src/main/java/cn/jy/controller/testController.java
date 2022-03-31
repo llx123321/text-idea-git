c7991003b01db9a52c061d04640100e217899a4b
llx <llx@163.com>
2022-03-31 17:55:33 +0800

    这是dev第一次提交

--- src/main/java/cn/jy/controller/testController.java
+++ src/main/java/cn/jy/controller/testController.java
@@ -3,5 +3,6 @@ package cn.jy.controller;
 public class testController {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git dev");
     }
 }
